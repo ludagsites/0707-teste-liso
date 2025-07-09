e94e1a71a2dbb1c3a0e29e369a0f05a1e50b886d
ludagsites <valdelayka@gmail.com>
2025-07-09 00:36:39 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import OrderPage from "./pages/OrderPage";
+import MyOrders from "./pages/MyOrders";
+import Auth from "./pages/Auth";
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminProducts from "./pages/AdminProducts";
+import AdminPizzaFlavors from "./pages/AdminPizzaFlavors";
+import AdminOrders from "./pages/AdminOrders";
+import AdminReports from "./pages/AdminReports";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/order" element={<OrderPage />} />
+          <Route path="/my-orders" element={<MyOrders />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/products" element={<AdminProducts />} />
+          <Route path="/admin/pizza-flavors" element={<AdminPizzaFlavors />} />
+          <Route path="/admin/orders" element={<AdminOrders />} />
+          <Route path="/admin/reports" element={<AdminReports />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
